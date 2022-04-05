2914f2165bfd909b52db5bc9a212cd1187e468ec
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-04-05 17:17:14 -0300

    Initial files, server and client

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import chalk from 'chalk';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
+
+const app = express();
+const port:number = 8087;
+const server = createServer(app)
+const io = new Server(server);
+const users = [];
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+io.on('connection', (socket) => {
+    console.log(chalk.green(`New client connected: ${socket.id}`));
+    io.emit(`connected`, socket.id);
+
+    socket.on('message', (x, y) => {
+        io.emit('message', x, y);
+    })
+})
+
+
+server.listen(port, () => {
+    console.log(chalk.black.bgGreen(`Server is running on port: ${port}`));
+})
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { io } from "socket.io-client";
+import readline from "readline";
+import chalk from "chalk";
+const messages = [];
+var user = null;
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
+
+const socket = io("http://localhost:8087");
+
+function sendMessage(message: string, user: string) {
+  socket.emit("message", { message, user });
+}
+
+function askForMessage() {
+  rl.question("", (message) => {
+
+    if (message == "/clear") {
+      console.clear();
+    }
+
+    if (!message.includes("/")) {
+      let dateOfMessage = new Date().toLocaleTimeString()
+
+      let MG = `${message}  ${chalk.dim(dateOfMessage)}`
+    
+      sendMessage(MG, user);
+    }
+
+    messages.push(message);
+
+    if (messages.indexOf(message) == 1) {
+      console.clear();
+    }
+
+    readline.moveCursor(process.stdout, 0, -2);
+    readline.clearScreenDown(process.stdout);
+
+    askForMessage();
+  });
+
+}
+
+socket.on("message", function (x) {
+  user ? console.log(`${chalk.blue(`${x.user} said:`)} ${x.message}`) : console.log('  someone sended a message, but you dont have a name yet, insert a name:');
+  
+});
+
+
+
+if(messages.length == 0 && user == null){
+    rl.question(`${chalk.red("What is your name?")}`, (name) => {
+        user = name;
+        console.clear()
+        console.log(`hey there ${user}`)
+        askForMessage();
+    })
+};
+
+
